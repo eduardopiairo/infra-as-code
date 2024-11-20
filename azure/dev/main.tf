327bcfdf86fc18e14ee3ddd598a86b2c3c62eaa8
eduardopiairo <eduardopiairo@gmail.com>
2024-11-20 18:38:34 +0000

    initial terraform module - resource group

---
+++ azure/dev/main.tf
@@ -0,0 +1,9 @@
+#######################################
+##      Module -  ResourceGroup     ###
+#######################################
+module "resource_group" {
+  source   = "../modules/resource_group"
+  name     = local.resource_group
+  location = local.location
+  tags     = local.tags
+}
---
+++ azure/dev/provider.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_version = "~> 1.9"
+  required_providers {
+
+    random = {
+      source  = "hashicorp/random"
+      version = "= 3.5.1"
+    }
+
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.54.0"
+    }
+    
+  }
+}
