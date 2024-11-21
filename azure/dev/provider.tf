eeec1aa4c6eacb0ba15ee3128d0e8a3a4dda6dc6
eduardopiairo <eduardopiairo@gmail.com>
2024-11-21 19:57:35 +0000

    terraform init working with env variables

--- azure/dev/provider.tf
+++ azure/dev/provider.tf
@@ -1,6 +1,11 @@
 terraform {
-  #required_version = "~> 1.9"
+  required_version = "~> 1.9"
   required_providers {
+
+    random = {
+      source  = "hashicorp/random"
+      version = "= 3.5.1"
+    }
 
     azurerm = {
       source  = "hashicorp/azurerm"
