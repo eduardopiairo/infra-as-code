593d1d20a3e2cf3d79b9497824e29c5af6c127e4
eduardopiairo <eduardopiairo@gmail.com>
2024-12-12 23:50:09 +0000

    tfstate respurce group update

--- azure/control-plane/backend.tf
+++ azure/control-plane/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "azurerm" {
-    resource_group_name  = "rg-platform-idp"
+    resource_group_name  = "rg-terraform-tfstate"
     storage_account_name = "storageplatformidp"
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
